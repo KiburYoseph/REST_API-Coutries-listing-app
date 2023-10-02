9055bfe9b5f03a87ecb763890efa5875c958bba1
KiburYoseph <kiburstgr@gmail.com>
2023-10-02 12:03:09 +0300

    Full menu Functional And Responsive

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,16 +1,35 @@
 import '../Components/Content.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
 
 const Content = () => {
 
     const [searchTerm, setSearchTerm] = useState('')
+    const [menuState, setMenuState] = useState(true)
+    const [filterState, setFilterState] = useState('none')
 
     return (
         <>
             <section className='menuSection'>
                 <div className='search'>
+                <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "hsl(0, 0%, 52%)"}} className='magnifyingGlass'/>
                     <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value )} placeholder='Search for a country...' />
                 </div>
+                <button className='dropDownBtn' onClick={() => setMenuState(!menuState)}>{(filterState == 'none') ?  "Filter by Region" : filterState}<FontAwesomeIcon icon={faCaretDown} style={{color: "#111517"}} className={`caretDown rotate${!menuState}`}/>
+                { menuState ? 
+                <ul className='dropDownMenu'>
+                        <li onClick={() => setFilterState('Africa')}>Africa</li>
+                        <li onClick={() => setFilterState('America')}>America</li>
+                        <li onClick={() => setFilterState('Asia')}>Asia</li>
+                        <li onClick={() => setFilterState('Europe')}>Europe</li>
+                        <li onClick={() => setFilterState('Oceania')}>Oceania</li>
+                        <li onClick={() => setFilterState('none')}>No Filter</li>
+                </ul>
+                    : null}    
+                    
+                </button>
             </section>
 
             <ul className='listSection'>
